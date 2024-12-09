52a008bd1f575cabdf9ab85681e5a1cb139bc424
andrek001 <andrewfml@gmail.com>
2024-12-09 23:56:44 +0300

    init commit

---
+++ src/main/java/com/netology/diplom/entity/CloudFile.java
@@ -0,0 +1,35 @@
+package com.netology.diplom.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cloud_file")
+public class CloudFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+
+    @Column(name ="size")
+    private Long size;
+
+    @Column(name = "data")
+    private byte[] data;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+}
